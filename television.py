97f0c7f5c2e7c8ba02fd98a44ccdb35b19c1652f
tunnelarmr <elijahthemoore@gmail.com>
2025-11-25 21:30:54 -0600

    added docstrings to television.py

--- television.py
+++ television.py
@@ -1,10 +1,25 @@
 class Television:
+    '''This function takes input from main.py and modifies the TV's power status, volume, and channel.
+    :param MIN_VOLUME: The minimum allowed volume value for the TV.
+    :param MAX_VOLUME: The maximum allowed volume value for the TV.
+    :param MIN_CHANNEL: The minimum allowed channel value for the TV.
+    :param MAX_CHANNEL: The maximum allowed channel value for the TV.
+    :return:
+    '''
     MIN_VOLUME = 0
     MAX_VOLUME = 2
     MIN_CHANNEL = 0
     MAX_CHANNEL = 3
 
     def __init__(self):
+        '''
+    def __init__ sets the initial values for the TV's power status, volume, and channel.
+
+    :param self._status: A private variable that sets the initial power status of the TV to on (True) or off (False)
+    :param self._muted: A private variable that sets the initial mute status of the TV to on (True) or off (False)
+    :param self._volume: A private variable that sets the initial volume status of the TV to the previously defined MIN_VOLUME
+    :param self._channel: A private variable that sets the initial channel status of the TV to the previously defined MIN_CHANNEL
+        '''
         self._status = False
         self._muted = False
         self._volume = Television.MIN_VOLUME
